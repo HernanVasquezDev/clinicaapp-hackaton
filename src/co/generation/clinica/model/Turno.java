bc4db835e6e3d0fed681d820a329d852492edc16
Hernan_Vasquez <nachjvasquez@gmail.com>
2026-05-08 10:33:59 -0500

    Done Turno module

--- src/co/generation/clinica/model/Turno.java
+++ src/co/generation/clinica/model/Turno.java
@@ -1,4 +1,91 @@
 package co.generation.clinica.model;
 
+import java.time.LocalDateTime;
+
 public class Turno {
+    private int id;
+    private Paciente paciente;
+    private Medico medico;
+    private LocalDateTime fechaHora;
+    private EstadoTurno estado;
+
+    //constructor CSV
+    public Turno(int id, Paciente paciente, Medico medico, LocalDateTime fechaHora, EstadoTurno estado) {
+        this.id = id;
+        this.paciente = paciente;
+        this.medico = medico;
+        this.fechaHora = fechaHora;
+        this.estado = estado != null ? estado : EstadoTurno.PENDIENTE;
+    }
+    //constructor consola
+    public Turno(Paciente paciente, Medico medico, LocalDateTime fechaHora, EstadoTurno estado) {
+        this.paciente = paciente;
+        this.medico = medico;
+        this.fechaHora = fechaHora;
+        this.estado = EstadoTurno.PENDIENTE;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        if(paciente == null){
+            throw new IllegalArgumentException("El campo paciente no puede estar vacio.");
+        }
+        this.paciente = paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        if(medico == null){
+            throw new IllegalArgumentException("El campo medico no puede estar vacio.");
+        }
+            this.medico = medico;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        if(fechaHora == null){
+            throw new IllegalArgumentException("El campo fechaHora no puede estar vacio.");
+        }
+        this.fechaHora = fechaHora;
+    }
+
+    public EstadoTurno getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoTurno estado) {
+        if(estado == null){
+            throw new IllegalArgumentException("El campo estado no puede estar vacio.");
+        }
+        this.estado = estado;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)return true;
+        if(!(obj instanceof Turno))return false;
+
+        Turno other = (Turno) obj;
+
+        return this.medico.equals(other.medico)
+            && this.fechaHora.equals(other.fechaHora);
+    }
+
 }
